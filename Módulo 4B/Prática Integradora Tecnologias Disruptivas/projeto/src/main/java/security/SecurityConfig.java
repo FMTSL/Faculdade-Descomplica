dc73cf722b699682308b3e55e42a8a216fb35b26
FMTSL <Felipe_Matos@hotmail.com.br>
2023-11-26 20:36:16 -0300

    Atividade 10

---
+++ Módulo 4B/Prática Integradora Tecnologias Disruptivas/projeto/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,7 @@
+package security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration//É uma classe de configuração, definir rotas públicas ou privadas
+public class SecurityConfig {
+}
